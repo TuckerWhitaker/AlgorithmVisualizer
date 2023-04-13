22ccef0f46016c3ffc55761b699c8549f0cf5cc6
Tucker Whitaker <trw2005.50@gmail.com>
2023-04-13 13:35:48 -0500

    timed quicksort

--- client/src/components/SortingAlgorithms.jsx
+++ client/src/components/SortingAlgorithms.jsx
@@ -4,13 +4,25 @@ import "./SortingAlgorithms.css";
 function SortingAlgorithms() {
 	let starttime = 0;
 
+	async function play(frequency = 300, duration = 1e3) {
+		const context = new AudioContext();
+		const gainNode = context.createGain();
+		const oscillator = context.createOscillator();
+		oscillator.frequency.value = frequency;
+		oscillator.connect(gainNode);
+		gainNode.connect(context.destination);
+		oscillator.start(0);
+		setTimeout(() => oscillator.stop(), duration);
+	}
+
 	function Delay(time) {
 		return new Promise((resolve) => setTimeout(resolve, time));
 	}
 	const [DisplayArray, SetDisplayArray] = useState([1]);
+	const [ArrayLength, SetArrayLength] = useState(50);
 
 	function End(time) {
-		console.log(time - starttime);
+		console.log((time - starttime) / 1000 + " seconds");
 	}
 	const [, updateState] = React.useState();
 	const forceUpdate = React.useCallback(() => updateState({}), []);
@@ -44,7 +56,10 @@ function SortingAlgorithms() {
 			"rgb(255, 0, 0)";
 	}
 	async function swap(arr, a, b) {
-		await Delay(25);
+		//console.log("ARR" + arr[a]);
+		//play(arr[a], 100);
+		//play(arr[b], 500);
+		await Delay(1);
 		forceUpdate();
 		let temp = arr[a];
 		arr[a] = arr[b];
@@ -55,7 +70,7 @@ function SortingAlgorithms() {
 		let pivotIndex = start;
 
 		let pivotValue = arr[end];
-		HighlightPivot(pivotIndex);
+		//HighlightPivot(pivotIndex);
 		for (let i = start; i < end; i++) {
 			if (arr[i] < pivotValue) {
 				await swap(arr, i, pivotIndex);
@@ -68,6 +83,10 @@ function SortingAlgorithms() {
 	}
 
 	async function quickSort(arr, start, end) {
+		if (start > arr.length - 2) {
+			console.log("QuickSort End");
+			End(Date.now());
+		}
 		if (start >= end) {
 			return;
 		}
@@ -90,12 +109,10 @@ function SortingAlgorithms() {
 							key={index}
 							style={{
 								height: info / 1 + "px",
-								width: 90 / DisplayArray.length + "vw",
+								width: "1px",
 								backgroundColor: color,
 							}}
-						>
-							{info}
-						</span>
+						></span>
 					);
 				})}
 			</div>
@@ -103,7 +120,7 @@ function SortingAlgorithms() {
 				<button
 					onClick={() => {
 						let temparray = [];
-						for (let i = 0; i < 100; i++) {
+						for (let i = 0; i < ArrayLength; i++) {
 							temparray.push(Math.round(Math.random() * 255));
 						}
 						SetDisplayArray(temparray);
@@ -113,21 +130,33 @@ function SortingAlgorithms() {
 				</button>
 				<button
 					onClick={() => {
-						starttime = Date.now();
 						//SetDisplayArray(mergeSort(DisplayArray));
 						//mergeSort(DisplayArray);
-						End(Date.now());
 					}}
 				>
 					Merge
 				</button>
 				<button
 					onClick={() => {
+						starttime = Date.now();
 						quickSort(DisplayArray, 0, DisplayArray.length - 1);
 					}}
 				>
 					QuickSort
 				</button>
+				<button
+					onClick={() => {
+						play(100, 1000);
+					}}
+				>
+					Play sound
+				</button>
+				<input
+					placeholder={ArrayLength}
+					onChange={(e) => {
+						SetArrayLength(e.target.value);
+					}}
+				></input>
 			</div>
 		</div>
 	);
