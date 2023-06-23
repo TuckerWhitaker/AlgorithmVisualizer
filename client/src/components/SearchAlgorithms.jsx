11bcdf461ba2b47c0196a68063ed38eefa211a6b
Tucker Whitaker <trw2005.50@gmail.com>
2023-06-23 14:52:36 -0500

    Search Vis improved

--- client/src/components/SearchAlgorithms.jsx
+++ client/src/components/SearchAlgorithms.jsx
@@ -4,7 +4,85 @@ function SearchAlgorithms() {
 	return (
 		<div className="SearchAlgorithms">
 			<div className="SearchAlgorithmsTitle">Search Algorithms</div>
-			<SearchVis></SearchVis>
+			<SearchVis
+				SearchFunction={async function LinearSearch(
+					array,
+					CodeHighlightIndex,
+					forceUpdate,
+					Delay,
+					delay
+				) {
+					for (let i = 0; i < array.length; i++) {
+						document.getElementById(
+							"Code" + CodeHighlightIndex
+						).style.backgroundColor = "rgba(49, 78, 136, 0)";
+						CodeHighlightIndex = 0;
+						document.getElementById(
+							"Code" + CodeHighlightIndex
+						).style.backgroundColor = "rgba(49, 78, 136, 1)";
+
+						document.getElementById("SearchVisNum" + i).style.backgroundColor =
+							"rgba(0, 150, 0, 1)";
+						if (i > 0) {
+							document.getElementById(
+								"SearchVisNum" + (i - 1)
+							).style.backgroundColor = "rgba(0, 150, 0, 0)";
+							document.getElementById("SearchVisNum" + (i - 1)).style.color =
+								"rgba(100,100,100,1)";
+						}
+
+						forceUpdate();
+						await Delay(delay);
+						document.getElementById(
+							"Code" + CodeHighlightIndex
+						).style.backgroundColor = "rgba(49, 78, 136, 0)";
+						CodeHighlightIndex = 1;
+						document.getElementById(
+							"Code" + CodeHighlightIndex
+						).style.backgroundColor = "rgba(49, 78, 136, 1)";
+
+						forceUpdate();
+						await Delay(delay);
+
+						if (array[i] == 9) {
+							document.getElementById(
+								"Code" + CodeHighlightIndex
+							).style.backgroundColor = "rgba(49, 78, 136, 0)";
+							CodeHighlightIndex = 2;
+							document.getElementById(
+								"Code" + CodeHighlightIndex
+							).style.backgroundColor = "rgba(49, 78, 136, 1)";
+
+							forceUpdate();
+							return array[i];
+						}
+						document.getElementById(
+							"Code" + CodeHighlightIndex
+						).style.backgroundColor = "rgba(49, 78, 136, 0)";
+						CodeHighlightIndex = 4;
+						document.getElementById(
+							"Code" + CodeHighlightIndex
+						).style.backgroundColor = "rgba(49, 78, 136, 1)";
+
+						forceUpdate();
+						await Delay(delay);
+					}
+				}}
+				Id={0}
+				Title="Linear Search"
+				CodeArray={[
+					{
+						Tab: 0,
+						string: "for (let i = 0; i < array.length; i++) {",
+						HighLight: 0,
+					},
+					{ Tab: 50, string: "if (array[i] == 9) { ", HighLight: 0 },
+					{ Tab: 100, string: "return array[i];", HighLight: 0 },
+					{ Tab: 50, string: "}", HighLight: 0 },
+					{ Tab: 0, string: "}", HighLight: 0 },
+				]}
+				Description="The Linear Search sequentially checks each element of the list until a match is found or the whole list has been searched"
+			></SearchVis>
 		</div>
 	);
 }
--- client/src/components/SearchVis.jsx
+++ client/src/components/SearchVis.jsx
@@ -16,79 +16,22 @@ function Delay(time) {
 	return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-function SearchVis() {
+function SearchVis(props) {
 	const [, updateState] = React.useState();
 	const forceUpdate = React.useCallback(() => updateState({}), []);
 
-	const [CodeArray, SetCodeArray] = useState([
-		{
-			Tab: 0,
-			string: "for (let i = 0; i < array.length; i++) {",
-			HighLight: 0,
-		},
-		{ Tab: 50, string: "if (array[i] == 9) { ", HighLight: 0 },
-		{ Tab: 100, string: "return array[i];", HighLight: 0 },
-		{ Tab: 50, string: "}", HighLight: 0 },
-		{ Tab: 0, string: "}", HighLight: 0 },
-	]);
-	const [NumHighlight, SetNumHighlight] = useState([0, 0, 0, 0, 0, 0, 0]);
-	var temparray = [];
-	var tempHighlight = [];
-	async function LinearSearch() {
-		for (let i = 0; i < array.length; i++) {
-			temparray = CodeArray;
-			tempHighlight = NumHighlight;
-			temparray[0].HighLight = 1;
-			temparray[1].HighLight = 0;
-			temparray[4].HighLight = 0;
-
-			tempHighlight[i - 1] = 0;
-			if (i > 0) {
-				document.getElementById("SearchVisNum" + (i - 1)).style.color =
-					"rgba(100,100,100,1)";
-			}
-
-			tempHighlight[i] = 1;
-			SetNumHighlight(tempHighlight);
-
-			SetCodeArray(temparray);
-			forceUpdate();
-			await Delay(delay);
-			temparray[0].HighLight = 0;
-			temparray[1].HighLight = 1;
-
-			SetCodeArray(temparray);
-			forceUpdate();
-			await Delay(delay);
-
-			if (array[i] == 9) {
-				temparray[1].HighLight = 0;
-				temparray[2].HighLight = 1;
-
-				SetCodeArray(temparray);
-				forceUpdate();
-				return array[i];
-			}
-			temparray[1].HighLight = 0;
-			temparray[2].HighLight = 0;
-			temparray[4].HighLight = 1;
-			SetCodeArray(temparray);
-			forceUpdate();
-			await Delay(delay);
-		}
-	}
+	let CodeHighlightIndex = 0;
 
 	return (
 		<div className="SearchVis">
-			<div className="SearchVisTitle">Linear Search</div>
+			<div className="SearchVisTitle">{props.Title}</div>
 			<div className="SearchVisNumberParent">
 				{array.map((info, index) => {
-					var tempColor = "rgba(0, 150, 0," + NumHighlight[index] + ")";
 					return (
 						<div
 							className="SearchVisNumber"
 							key={index}
-							style={{ backgroundColor: tempColor }}
+							style={{ backgroundColor: "rgba(0, 150, 0, 0)" }}
 							id={"SearchVisNum" + index}
 						>
 							{info}
@@ -97,15 +40,13 @@ function SearchVis() {
 				})}
 			</div>
 			<div className="SearchVisInfo">
-				<div className="SearchVisInfoHalf">
-					The Linear Search sequentially checks each element of the list until a
-					match is found or the whole list has been searched
-				</div>
+				<div className="SearchVisInfoHalf">{props.Description}</div>
 				<div className="SearchVisInfoHalf SearchVisCode">
-					{CodeArray.map((info, index) => {
+					{props.CodeArray.map((info, index) => {
 						var tempColor = "rgba(49, 78, 136," + info.HighLight + ")";
 						return (
 							<div
+								id={"Code" + index}
 								className="CodeLine"
 								key={index}
 								style={{ marginLeft: info.Tab, backgroundColor: tempColor }}
@@ -118,7 +59,14 @@ function SearchVis() {
 			</div>
 			<button
 				onClick={() => {
-					LinearSearch();
+					//LinearSearch();
+					props.SearchFunction(
+						array,
+						CodeHighlightIndex,
+						forceUpdate,
+						Delay,
+						delay
+					);
 				}}
 			>
 				Search
