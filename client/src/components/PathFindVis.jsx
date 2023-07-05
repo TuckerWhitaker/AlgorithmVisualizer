c1ede104fed0537b0aa7d4c6275aa7170091dee6
Tucker Whitaker <trw2005.50@gmail.com>
2023-07-05 12:05:29 -0500

    Depth First Search

--- client/src/components/PathFindVis.jsx
+++ client/src/components/PathFindVis.jsx
@@ -1,4 +1,216 @@
+import React, { useEffect } from "react";
 function PathFindVis() {
+	function Delay(time) {
+		return new Promise((resolve) => setTimeout(resolve, time));
+	}
+
+	let grid = [];
+	let stack = [];
+
+	useEffect(() => {
+		GenerateMaze();
+	});
+
+	const GenerateMaze = () => {
+		grid = [];
+		stack = [];
+
+		for (let i = 0; i < 900; i++) {
+			grid.push({
+				hasvisited: false,
+				walls: [1, 1, 1, 1],
+				Index: i,
+			});
+		}
+
+		stack.push(grid[0]);
+
+		while (stack.length > 0) {
+			let currentCell = stack[stack.length - 1];
+			currentCell.hasvisited = true;
+
+			// Get all unvisited neighbors
+			let neighbors = [];
+			// Assuming a 30x30 grid
+			let index = grid.indexOf(currentCell);
+			if (index - 30 >= 0 && !grid[index - 30].hasvisited)
+				neighbors.push(grid[index - 30]); // Top
+			if ((index + 1) % 30 != 0 && !grid[index + 1].hasvisited)
+				neighbors.push(grid[index + 1]); // Right
+			if (index + 30 < 900 && !grid[index + 30].hasvisited)
+				neighbors.push(grid[index + 30]); // Bottom
+			if (index % 30 != 0 && !grid[index - 1].hasvisited)
+				neighbors.push(grid[index - 1]); // Left
+
+			// If there are unvisited neighbors
+			if (neighbors.length > 0) {
+				// Choose a random unvisited neighbor
+				let neighbor = neighbors[Math.floor(Math.random() * neighbors.length)];
+				// Push it to the stack
+				stack.push(neighbor);
+
+				// Remove the walls between the current cell and chosen cell
+
+				if (neighbor === grid[index - 30]) {
+					// Top
+					currentCell.walls[0] = 0;
+					neighbor.walls[2] = 0;
+				} else if (neighbor === grid[index + 1]) {
+					// Right
+					currentCell.walls[1] = 0;
+					neighbor.walls[3] = 0;
+				} else if (neighbor === grid[index + 30]) {
+					// Bottom
+					currentCell.walls[2] = 0;
+					neighbor.walls[0] = 0;
+				} else if (neighbor === grid[index - 1]) {
+					// Left
+					currentCell.walls[3] = 0;
+					neighbor.walls[1] = 0;
+				}
+			} else {
+				// Pop a cell from the stack
+				stack.pop();
+			}
+		}
+
+		let ctx = document.getElementById("PathFindVisCanvas").getContext("2d");
+
+		ctx.lineWidth = 2;
+
+		ctx.fillStyle = "#FFFFFF";
+		ctx.fillRect(0, 0, 320, 320);
+		ctx.fillStyle = "#000000";
+
+		for (let i = 10; i < 320 - 10; i += 10) {
+			for (let j = 10; j < 320 - 10; j += 10) {
+				// Calculate cell index from x and y
+				let x = (i - 10) / 10;
+				let y = (j - 10) / 10;
+				let index = y * 30 + x;
+
+				ctx.strokeStyle = "rgba(0, 0, 0, 2)";
+
+				// Draw top wall
+				if (grid[index].walls[0]) {
+					ctx.beginPath();
+					ctx.moveTo(i, j);
+					ctx.lineTo(i + 10, j);
+					ctx.stroke();
+				}
+
+				// Draw right wall
+				if (grid[index].walls[1]) {
+					ctx.beginPath();
+					ctx.moveTo(i + 10, j);
+					ctx.lineTo(i + 10, j + 10);
+					ctx.stroke();
+				}
+
+				// Draw bottom wall
+				if (grid[index].walls[2]) {
+					ctx.beginPath();
+					ctx.moveTo(i, j + 10);
+					ctx.lineTo(i + 10, j + 10);
+					ctx.stroke();
+				}
+
+				// Draw left wall
+				if (grid[index].walls[3]) {
+					ctx.beginPath();
+					ctx.moveTo(i, j);
+					ctx.lineTo(i, j + 10);
+					ctx.stroke();
+				}
+			}
+		}
+
+		ctx.fillStyle = "#00FF00";
+		ctx.fillRect(11, 11, 8, 8);
+		ctx.fillStyle = "#0000FF";
+		ctx.fillRect(301, 301, 8, 8);
+		for (let i = 0; i < grid.length; i++) {
+			grid[i].hasvisited = false;
+		}
+	};
+
+	async function Solve() {
+		let ctx = document.getElementById("PathFindVisCanvas").getContext("2d");
+		let stack = [];
+		let current = grid[0];
+		stack.push(grid[0]);
+
+		while (current.Index != 899) {
+			let currentCell = stack[stack.length - 1];
+
+			current = currentCell;
+
+			currentCell.hasvisited = true;
+
+			let neighbors = [];
+
+			let index = grid.indexOf(currentCell);
+
+			if (
+				index - 30 >= 0 &&
+				!grid[index - 30].hasvisited &&
+				grid[index].walls[0] == 0
+			)
+				neighbors.push(grid[index - 30]); // Top
+			if (
+				(index + 1) % 30 != 0 &&
+				!grid[index + 1].hasvisited &&
+				grid[index].walls[1] == 0
+			)
+				neighbors.push(grid[index + 1]); // Right
+			if (
+				index + 30 < 900 &&
+				!grid[index + 30].hasvisited &&
+				grid[index].walls[2] == 0
+			)
+				neighbors.push(grid[index + 30]); // Bottom
+			if (
+				index % 30 != 0 &&
+				!grid[index - 1].hasvisited &&
+				grid[index].walls[3] == 0
+			)
+				neighbors.push(grid[index - 1]); // Left
+
+			if (neighbors.length > 0) {
+				let neighbor = neighbors[Math.floor(Math.random() * neighbors.length)];
+
+				stack.push(neighbor);
+			} else {
+				// Pop a cell from the stack
+				stack.pop();
+			}
+		}
+
+		ctx.fillStyle = "#FF0000";
+		ctx.strokeStyle = "#FF0000";
+		let lastCell = [stack[0]].Index;
+
+		stack.pop();
+		for (let s = 0; s < stack.length; s++) {
+			await Delay(50);
+
+			ctx.beginPath();
+			ctx.moveTo(
+				(lastCell % 30) * 10 + 15,
+				Math.floor(lastCell / 30) * 10 + 15
+			);
+			ctx.lineTo(
+				(stack[s].Index % 30) * 10 + 15,
+				Math.floor(stack[s].Index / 30) * 10 + 15
+			);
+			ctx.stroke();
+
+			lastCell = stack[s].Index;
+		}
+		console.log("Solved");
+		//DFS
+	}
+
 	return (
 		<div className="PathFindVis">
 			<div className="PathFindVisTitle">Title</div>
@@ -14,122 +226,8 @@ function PathFindVis() {
 			</div>
 			<button
 				onClick={() => {
-					let grid = [];
-					let stack = [];
-
-					for (let i = 0; i < 900; i++) {
-						grid.push({ hasvisited: false, walls: [1, 1, 1, 1] });
-					}
-
-					stack.push(grid[0]);
-
-					while (stack.length > 0) {
-						let currentCell = stack[stack.length - 1];
-						currentCell.hasvisited = true;
-
-						// Get all unvisited neighbors
-						let neighbors = [];
-						// Assuming a 30x30 grid
-						let index = grid.indexOf(currentCell);
-						if (index - 30 >= 0 && !grid[index - 30].hasvisited)
-							neighbors.push(grid[index - 30]); // Top
-						if ((index + 1) % 30 != 0 && !grid[index + 1].hasvisited)
-							neighbors.push(grid[index + 1]); // Right
-						if (index + 30 < 900 && !grid[index + 30].hasvisited)
-							neighbors.push(grid[index + 30]); // Bottom
-						if (index % 30 != 0 && !grid[index - 1].hasvisited)
-							neighbors.push(grid[index - 1]); // Left
-
-						// If there are unvisited neighbors
-						if (neighbors.length > 0) {
-							// Choose a random unvisited neighbor
-							let neighbor =
-								neighbors[Math.floor(Math.random() * neighbors.length)];
-							// Push it to the stack
-							stack.push(neighbor);
-
-							// Remove the walls between the current cell and chosen cell
-
-							if (neighbor === grid[index - 30]) {
-								// Top
-								currentCell.walls[0] = 0;
-								neighbor.walls[2] = 0;
-							} else if (neighbor === grid[index + 1]) {
-								// Right
-								currentCell.walls[1] = 0;
-								neighbor.walls[3] = 0;
-							} else if (neighbor === grid[index + 30]) {
-								// Bottom
-								currentCell.walls[2] = 0;
-								neighbor.walls[0] = 0;
-							} else if (neighbor === grid[index - 1]) {
-								// Left
-								currentCell.walls[3] = 0;
-								neighbor.walls[1] = 0;
-							}
-						} else {
-							// Pop a cell from the stack
-							stack.pop();
-						}
-					}
-
-					let ctx = document
-						.getElementById("PathFindVisCanvas")
-						.getContext("2d");
-
-					ctx.lineWidth = 2;
-
-					ctx.fillStyle = "#FFFFFF";
-					ctx.fillRect(0, 0, 320, 320);
-					ctx.fillStyle = "#000000";
-
-					for (let i = 10; i < 320 - 10; i += 10) {
-						for (let j = 10; j < 320 - 10; j += 10) {
-							// Calculate cell index from x and y
-							let x = (i - 10) / 10;
-							let y = (j - 10) / 10;
-							let index = y * 30 + x;
-
-							ctx.strokeStyle = "rgba(0, 0, 0, 2)";
-
-							// Draw top wall
-							if (grid[index].walls[0]) {
-								ctx.beginPath();
-								ctx.moveTo(i, j);
-								ctx.lineTo(i + 10, j);
-								ctx.stroke();
-							}
-
-							// Draw right wall
-							if (grid[index].walls[1]) {
-								ctx.beginPath();
-								ctx.moveTo(i + 10, j);
-								ctx.lineTo(i + 10, j + 10);
-								ctx.stroke();
-							}
-
-							// Draw bottom wall
-							if (grid[index].walls[2]) {
-								ctx.beginPath();
-								ctx.moveTo(i, j + 10);
-								ctx.lineTo(i + 10, j + 10);
-								ctx.stroke();
-							}
-
-							// Draw left wall
-							if (grid[index].walls[3]) {
-								ctx.beginPath();
-								ctx.moveTo(i, j);
-								ctx.lineTo(i, j + 10);
-								ctx.stroke();
-							}
-						}
-					}
-
-					ctx.fillStyle = "#00FF00";
-					ctx.fillRect(11, 11, 8, 8);
-					ctx.fillStyle = "#0000FF";
-					ctx.fillRect(301, 301, 8, 8);
+					GenerateMaze();
+					Solve();
 				}}
 			>
 				Maze
